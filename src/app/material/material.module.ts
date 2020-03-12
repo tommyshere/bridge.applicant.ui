9519e67f710a68f0ba8fa01c48cd28cc6f71064b
Tommy <tommyshere@gmail.com>
2020-03-11 19:48:09 -0700

    First look

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     MatIconModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [
     MatIconModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
