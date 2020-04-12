58a256a63c20bd0c3f97d84e44fd52598293cb06
Tommy <tommyshere@gmail.com>
2020-04-12 12:23:24 -0700

    init currenty and user profile

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { CurrencyComponent } from './navbar/currency/currency.component';
+import { UserProfileComponent } from './navbar/user-profile/user-profile.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    CurrencyComponent,
+    UserProfileComponent
   ],
   imports: [
     SharedModule
